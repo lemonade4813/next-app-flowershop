c120ff98c7b7ce7545bcc8558408f57b4b605b23
jwlee <lemonade4813@gmail.com>
2024-01-18 00:20:50 +0900

    fix : 일자별 하훼 경매 정보 API 요청 헤더 설정(cors 이슈 해결)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,24 +5,34 @@ import Image from 'next/image'
 import { useEffect } from 'react'
 
 
-const flowerList = async () => {
-  
-  const response = await fetch(`https://flower.at.or.kr/api/returnData.api?
-                                kind=f001&
-                                serviceKey=FFA4DBD82C754CD29E5F7881DDA8F4D7&
-                                baseDate=2024-01-15&
-                                flowerGubn=1&
-                                dataType=json&
-                                countPerPage=40`, {cache : 'no-store'})
-        const data = await response.json()
 
-        console.log(data)
-}
 
 
 
 
 export default function Home() {
+
+
+  const flowerList = async () => {
+  
+    const response = await fetch('https://cors-anywhere.herokuapp.com/https://flower.at.or.kr/api/returnData.api?kind=f001&serviceKey=FFA4DBD82C754CD29E5F7881DDA8F4D7&baseDate=2024-01-15&flowerGubn=1&dataType=json&countPerPage=40', 
+    { method: 'GET',
+      headers : {
+        'Origin' : 'http://192.168.45.127',
+        'Cache-Control': 'no-store'
+      }
+    });
+          
+    
+    console.log(response)
+
+    const data = await response.json()
+
+          console.log(data)
+          
+  }
+
+
 
   useEffect(()=>{
     flowerList();
